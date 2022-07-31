3e18f9f9c6754c448ebd18016c1abfc317c6167c
Te-je Rodgers <tjd.rodgers@gmail.com>
2022-07-31 16:22:27 +0200

    update StaticGrappler and docs

--- grappler/grapplers/_static.py
+++ grappler/grapplers/_static.py
@@ -5,20 +5,36 @@ from grappler._types import Extension, Package, UnknownExtensionError
 
 
 class StaticGrappler:
-    @staticmethod
-    def internal_package() -> Package:
-        return Package(
-            "Static Extensions",
-            "0.0.0",
-            "grappler.grapplers.static.internal-package",
-            None,
-        )
+    """
+    A grappler for loading "extensions" supplied by the host
+    application.
+
+    This is provided as a useful tool to help modularize application
+    code, so that application components can be loaded in the same
+    way as extensions. To use this, supply an object as well as
+    topics that each object implements to either `__init__`
+    or `add_extension`, and the grappler will generate the appropriate
+    [`Extension`][grappler.Extension] tuples.
+
+    The `extension.package` is the same for every extension yielded
+    by an instance of this grappler. If a `package` argument is provided to the
+    constructor, then this is used. Otherwise, a default internal
+    package is used (`StaticGrappler.internal_package`).
+
+    """
+
+    internal_package = Package(
+        "Static Extensions",
+        "0.0.0",
+        "grappler.grapplers.static.internal-package",
+        None,
+    )
 
     def __init__(
         self, *objs: Tuple[Collection[str], Any], package: Optional[Package] = None
     ) -> None:
         self.id = "grappler.grapplers.static"
-        self.package = package or self.internal_package()
+        self.package = package or self.internal_package
 
         self.cache = {
             Extension(self.id, str(uuid4()), self.package, tuple(topics)): obj
